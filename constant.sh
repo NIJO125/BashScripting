65333e877549d67920a6d5ce7b1ccb90559f39f4
Nijo Luca <nijoluca125@gmail.com>
2020-09-07 17:34:09 +0000

    New commit

---
+++ constant.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+readonly text="My Name is Nijo Luca"
+new="I am very Happy "
+declare -r number=30
+echo $text
+echo $number
+echo $new
+text="My New Name Appu"
+new="I am so sad"
+echo $new
--- main.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-fruit="Apple"
-quantity=10
-echo "Fruit Name=$fruit,How many=$quantity"
-unset fruit
-echo "Fruit Name=$fruit,How many=$quantity"
