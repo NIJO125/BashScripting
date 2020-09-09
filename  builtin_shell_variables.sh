e18f326a53f45fa59fbf44e5f64a45d526263ac3
Nijo Luca <nijoluca125@gmail.com>
2020-09-09 09:50:37 +0000

    new commit

---
+++  builtin_shell_variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "MY Current Bash Path - $BASH"
+echo "My Bash Version -$BASH_VERSION"
+echo "PID of My Bash-$BASHPID"
+echo "My home directory-$HOME"
+echo "Where i am right Now-$PWD"
+echo "My hostname-$HOSTNAME"
